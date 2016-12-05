9634097a27bfd6a620364b2b1bf7106439c59c45
FengKeyuan <724300838@qq.com>
2016-12-05 11:00:01 +0800

    Initial Commit

---
+++ MINAFrameworkOfTCP/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MINAFrameworkOfTCP
+//
+//  Created by lccx on 2016/12/5.
+//  Copyright © 2016年 lccx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MINAFrameworkOfTCP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MINAFrameworkOfTCP
+//
+//  Created by lccx on 2016/12/5.
+//  Copyright © 2016年 lccx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
